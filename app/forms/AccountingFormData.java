8261b8689b02ad0c5220b19eda27e5c33207cce3
fleporcq <fleporcq@gmail.com>
2014-02-09 11:50:40 +0100

    gestion de la validation du formulaire

--- app/forms/AccountingFormData.java
+++ app/forms/AccountingFormData.java
@@ -1,10 +1,36 @@
 package forms;
 
+import models.Accounting;
 import play.data.validation.Constraints;
+import play.data.validation.ValidationError;
+import play.i18n.Messages;
+import utils.DateUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountingFormData {
 
     @Constraints.Required
     public int year;
+
+    public List<ValidationError> validate() {
+        List<ValidationError> errors = new ArrayList<ValidationError>();
+
+        if(Accounting.findByYear(year) != null){
+            errors.add(new ValidationError("year", Messages.get("error.accounting.year.alreadyExist")));
+        }
+
+        if(!DateUtils.checkYear(year)){
+            errors.add(new ValidationError("year", Messages.get("error.accounting.year.invalid")));
+        }
+
+        if (errors.size() > 0) {
+            return errors;
+        }
+
+        return null;
+    }
+
 
 }
