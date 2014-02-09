f3cf219d13add9b13bb2b1625efb3cbf0fd238df
fleporcq <fleporcq@gmail.com>
2014-02-09 18:25:20 +0100

    gestion des l'export partiel

--- app/controllers/ExportController.java
+++ app/controllers/ExportController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.*;
+import play.i18n.Messages;
 import play.mvc.Controller;
 import play.mvc.Result;
 import util.pdf.PDF;
@@ -11,7 +12,8 @@ import views.html.ExportController.pdf;
 import java.util.List;
 
 public class ExportController extends Controller {
-    public static Result pdf(int year) {
+
+    public static Result pdf(int year, int month, boolean onlySummary) {
         if (!DateUtils.checkYear(year)) {
             return notFound();
         }
@@ -22,10 +24,45 @@ public class ExportController extends Controller {
         List<ParentCategoryList> expensePages = new ParentCategoryList(Category.findParents(ERowType.EXPENSE)).paginate(12, 9);
         List<Treasury> recipeTreasuries = Treasury.findByType(ERowType.RECIPE);
         List<ParentCategoryList> recipePages = new ParentCategoryList(Category.findParents(ERowType.RECIPE)).paginate(12, 9);
-        return PDF.ok(pdf.render(year, recipePages, recipeTreasuries, expensePages, expenseTreasuries));
+        return PDF.ok(pdf.render(year, month, onlySummary, recipePages, recipeTreasuries, expensePages, expenseTreasuries));
     }
 
-    public static Result export(int year) {
-        return ok(export.render(year));
+    public static Result saveAsPdf(int year, int month, boolean onlySummary) {
+        User user = Secured.getConnectedUser();
+
+        String partialName = "";
+        if(month > 0 && !onlySummary){
+            partialName = "-" + month;
+        }else if(onlySummary){
+            partialName = "-" + Messages.get("pdf.filename.summary");
+        }
+
+        response().setContentType("application/x-download");
+        response().setHeader("Content-disposition","attachment; filename=" + Messages.get("pdf.filename.prefix") +"-" + user.username + "-" + year + partialName + ".pdf");
+        return pdf(year, month, onlySummary);
+    }
+
+    public static Result exportYear(int year) {
+        if (!DateUtils.checkYear(year)) {
+            return notFound();
+        }
+        return ok(export.render(year, 0, false));
+    }
+
+    public static Result exportMonth(int year, int month) {
+        if (!DateUtils.checkYear(year)) {
+            return notFound();
+        }
+        if (!DateUtils.checkMonth(month)) {
+            return notFound();
+        }
+        return ok(export.render(year, month, false));
+    }
+
+    public static Result exportSummary(int year) {
+        if (!DateUtils.checkYear(year)) {
+            return notFound();
+        }
+        return ok(export.render(year, 0, true));
     }
 }
